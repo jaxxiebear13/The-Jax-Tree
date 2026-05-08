4a0d83b2ac17678fd8a5acb6047d0cc77b5c5af4
jaxxiebear13 <157326388+jaxxiebear13@users.noreply.github.com>
2026-05-08 13:54:55 -0400

    slight change to mod.js

--- js/mod.js
+++ js/mod.js
@@ -22,7 +22,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Yeah<br>
 		- DISCORD INVITE LINK ONLY HAS 50 USES.`
 
-let winText = `Congratulations! You won!`
+let winText = `Congratulations! You won! That's cool!`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
