5d046cdd8bbc00322754e806694a9b47788b2219
jaxxiebear13 <157326388+jaxxiebear13@users.noreply.github.com>
2026-05-08 14:09:59 -0400

    What

--- js/game.js
+++ js/game.js
@@ -163,10 +163,10 @@ function layerDataReset(layer, keep = []) {
 
 
 function addPoints(layer, gain) {
-	player[layer].points = player[layer].points.add(gain).max(0)
 	// easier readability for upgrades cause im not very good at spotting stuff
 	if (hasUpgrade('r', 11)) gain = gain.times(2)
 	//
+	player[layer].points = player[layer].points.add(gain).max(0)
 	if (player[layer].best) player[layer].best = player[layer].best.max(player[layer].points)
 	if (player[layer].total) player[layer].total = player[layer].total.add(gain)
 }
