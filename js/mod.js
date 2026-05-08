fe60f949e5bb3e4d32b07948dd7d95343dd7f406
jaxxiebear13 <157326388+jaxxiebear13@users.noreply.github.com>
2026-05-08 18:40:18 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -74,7 +74,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.p.points.gte(new Decimal("5")) && hasMilestone('p', 0)
+	return player.p.points.gte(new Decimal("15")) && hasMilestone('p', 1) && hasAchievement('ach', 16)
 }
 
 
