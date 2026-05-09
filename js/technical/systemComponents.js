720586455ac8b07a8d59750b781e24d94237d48e
jaxxiebear13 <157326388+jaxxiebear13@users.noreply.github.com>
2026-05-08 21:03:25 -0400

    Update systemComponents.js

--- js/technical/systemComponents.js
+++ js/technical/systemComponents.js
@@ -173,6 +173,7 @@ var systemComponents = {
 				</tr> 
 			<tr>
                 <td><button class="opt" onclick="toggleOpt('hideMilestonePopups')">Show Milestone Popups: {{ formatOption(!options.hideMilestonePopups) }}</button></td>
+				<td><button class="opt" onclick="toggleOpt('toggleMusic')">Music: {{ formatOption(options.music) }}</button></td>
             </tr>
         </table>`
     },
